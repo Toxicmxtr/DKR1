8c3f63a73e817ac38a0cb2430e3a8265ad571313
Данила <117807019+Toxicmxtr@users.noreply.github.com>
2022-11-09 11:04:10 +0300

    Add files via upload

---
+++ Gubkin_Danil_Ivanovich_ISPk101-51-00_DKR.pas
@@ -0,0 +1,14 @@
+﻿var x:real; 
+begin
+  x:=-9;
+  while x<=-3 do
+  begin
+    if x<-7 then
+      writeln('x=',x:1:2,'; e= Не вычесляется');
+    if (x>=-7) and (x<-5) then
+      writeln('x=',x:1:2,'; e=',(x*sqr(x)+23):1:2);
+    if x>=-5 then
+      writeln('x=',x:1:2,'; e= Не вычесляется');
+    x:=x+0.2;
+  end;
+end.
